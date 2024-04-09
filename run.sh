fe8915604f06265c62d3add7185f13f7125f3204
Sasanka Kuruppuarachchi <sasa.kuruppuarachchi@gmail.com>
2024-04-09 14:31:43 +0100

    run.sh file

--- run.sh
+++ run.sh
@@ -1,129 +1,71 @@
 #!/bin/bash
 
-tmux has-session -t nef_issac_vio
+tmux has-session -t agipix_test
 if [ $? == 0 ]; then
-    tmux kill-session -t nef_issac_vio
+    tmux kill-session -t agipix_test
 fi
-tmux new-session -s nef_issac_vio -n main -d # new session
-tmux new-window -n debug -t nef_issac_vio # new window
-tmux new-window -n sim -t nef_issac_vio # new window
-tmux new-window -n gazebo -t nef_issac_vio # new window
-tmux new-window -n yolact_test -t nef_issac_vio # new window
-tmux new-window -n traj_eval -t nef_issac_vio # new window
+tmux new-session -s agipix_test -n main -d # new session
+tmux new-window -n debug -t agipix_test # new window
+tmux new-window -n dds -t agipix_test # new window
+tmux new-window -n v-slam -t agipix_test # new window
+tmux new-window -n sensor_test -t agipix_test # new window
 
 tmux select-window -t main
 
 # divide
 tmux split-window -h -t 1
-
-# divide
 tmux split-window -v -t 1
-tmux split-window -v -t 3
-tmux split-window -v -t 4
-
-tmux split-window -v -t 5
-
-tmux resize-pane -D -t 1 5
 
 # run
 sleep 1
-tmux send-keys -t 6 "roscore" C-m
+tmux send-keys -t 1 "" C-m
 #tmux send-keys -t 3 "ISAAC_PY sim_launch.py" C-m
 
 #OV
-sleep 1
-tmux send-keys -t 1 "cd ~/workspace/arrf_exp/" C-m
-tmux send-keys -t 1 "sorthis" C-m
-tmux send-keys -t 1 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 1 "roslaunch ov_msckf subscribe_nrf.launch" 
 
-#VinsFUSION
-tmux send-keys -t 2 "ISAAC_ROS_WS" C-m
-tmux send-keys -t 2 "sorthis" C-m
-tmux send-keys -t 2 "rosparam set use_sim_time true" C-m
-#tmux send-keys -t 2 "roslaunch isaac_vins new.launch" 
-tmux send-keys -t 2 "nrf" C-m
-tmux send-keys -t 2 "cd tests/bags/x_movement" C-m
-tmux send-keys -t 2 "rosbag record /ground_truth/state /mavros/local_position/pose /mavros/setpoint_position/local /ov_msckf/poseimu /ov_msckf1/poseimu"
+tmux send-keys -t 2 "" C-m
 
-#teleop
-tmux send-keys -t 4 "rosparam set use_sim_time true" C-m
-#tmux send-keys -t 4 "rosrun teleop_twist_keyboard teleop_twist_keyboard.py" 
-tmux send-keys -t 4 "cd tests/bags/scenes" C-m
-#teleop
-tmux send-keys -t 5 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 5 "rviz -d ~/workspace/arrf_exp/src/sa-slam_indoor_aerial_nav_for_warehouses/open_vins/ov_msckf/launch/display_final.rviz" 
 
-#recode
-tmux send-keys -t 3 "cd ~/workspace/arrf_exp/" C-m
-tmux send-keys -t 3 "sorthis" C-m
-tmux send-keys -t 3 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 3 "roslaunch ov_msckf recode.launch"
 
 
 #simulation
-tmux select-window -t sim
+tmux select-window -t dds
+tmux split-window -v -t 1
+tmux send-keys -t 1 "cd /workspaces/dds/Micro-XRCE-DDS-Agent" C-m
+tmux send-keys -t 1 "cd build" C-m
+tmux send-keys -t 1 "cmake .." C-m
+tmux send-keys -t 1 "make" C-m
+tmux send-keys -t 1 "sudo make install" C-m
+tmux send-keys -t 1 "sudo ldconfig /usr/local/lib/" C-m
+tmux send-keys -t 1 "sudo MicroXRCEAgent serial --dev /dev/ttyUSB0 -b 921600" C-m
+sleep 1
 
-tmux send-keys -t 1 "ISAAC_CD" C-m
-tmux send-keys -t 1 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 1 "./python.sh standalone_examples/api/omni.isaac.quadruped/carter_stdalone.py"
+tmux send-keys -t 2 "cd /workspaces/px4_ros2" C-m
+tmux send-keys -t 2 "source install/setup.bash" C-m
+tmux send-keys -t 2 "ros2 launch px4_ros_com sensor_combined_listener.launch.py" C-m
 
 #eval
-tmux select-window -t traj_eval
+tmux select-window -t sensor_test
 
-tmux send-keys -t 1 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 1 "cd ~/workspace/arrf_exp/" C-m
-tmux send-keys -t 1 "sorthis" C-m
-tmux send-keys -t 1 "cd src/sa-slam_indoor_aerial_nav_for_warehouses/tests/loop" C-m
-tmux send-keys -t 1 "rosrun ov_eval error_comparison posyaw truth/ algorithms/"
+tmux send-keys -t 1 "cd /workspaces/isaac_ros-dev" C-m
+tmux send-keys -t 1 "colcon build --symlink-install" C-m
+tmux send-keys -t 1 "source install/setup.bash" C-m
+tmux send-keys -t 1 "ros2 launch realsense2_camera rs_launch.py pointcloud.enable:=True enable_infra1:=True enable_infra2:=True enable_gyro:=True enable_accel:=True" C-m
 
 #debug
 tmux select-window -t debug
-tmux send-keys -t 1 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 1 "cd ~/workspace/arrf_exp/" C-m
-tmux send-keys -t 1 "sorthis" C-m
-
-#gazebo
-tmux select-window -t gazebo
-tmux split-window -v -t 1
-tmux split-window -h -t 1
-tmux split-window -v -t 2
-
-
-tmux send-keys -t 1 "cd ~/workspace/arrf_exp/" C-m
-tmux send-keys -t 1 "sorthis" C-m
-#source ~/catkin_ws/devel/setup.bash
-tmux send-keys -t 1 "source /home/arrf/PX4-Autopilot/Tools/simulation/gazebo-classic/setup_gazebo.bash /home/arrf/PX4-Autopilot/ /home/arrf/PX4-Autopilot/build/px4_sitl_default" C-m
-tmux send-keys -t 1 "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/arrf/PX4-Autopilot/Tools/simulation/gazebo-classic/sitl_gazebo-classic:/home/arrf/PX4-Autopilot/" C-m
-#tmux send-keys -t 1 "export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:/home/arrf/PX4-Autopilot/" C-m
-tmux send-keys -t 1 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 1 "roslaunch nrf_warehouse warehouse.launch"
-#tmux send-keys -t 1 "roslaunch dynamic_logistics_warehouse logistics_warehouse.launch"  
-
-
-tmux send-keys -t 4 "cd ~/workspace/arrf_exp/" C-m
-tmux send-keys -t 4 "sorthis" C-m
-tmux send-keys -t 4 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 4 "rosrun offboard nrf.py"
-
-
-tmux send-keys -t 2 "cd ~/workspace/arrf_exp/src/sa-slam_indoor_aerial_nav_for_warehouses/tests/bags" C-m
-tmux send-keys -t 2 "rosparam set use_sim_time true" C-m
-#tmux send-keys -t 2 "rosbag record /ground_truth/state /mavros/local_position/pose /mavros/setpoint_position/local /ov_msckf/poseimu /ov_msckf1/poseimu"
-tmux send-keys -t 2 "rosbag record /ground_truth/state /mavros/local_position/pose /mavros/setpoint_position/local /mavros/imu/data /stereo/left/image_raw /stereo/right/image_raw /iris/camera/depth/image_raw /clock"
-
-tmux send-keys -t 3 "cd ~/workspace/arrf_exp/src/sa-slam_indoor_aerial_nav_for_warehouses/tests/bags/scenes" C-m
-tmux send-keys -t 3 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 3 "rosbag play xy_movement.bag"
+tmux send-keys -t 1 "cd /workspaces/px4_ros2" C-m
+tmux send-keys -t 1 "source install/setup.bash" C-m
+tmux send-keys -t 1 "ros2 topic list" C-m
 
 #yolact_test
-tmux select-window -t yolact_test
-tmux send-keys -t 1 "cd ~/workspace/arrf_exp/" C-m
-tmux send-keys -t 1 "sorthis" C-m
-tmux send-keys -t 1 "rosparam set use_sim_time true" C-m
-tmux send-keys -t 1 "rosrun yolact_ros sa_vio_outlier_detector_yedge"
+tmux select-window -t v-slam
+tmux send-keys -t 1 "cd /workspaces/isaac_ros-dev" C-m
+tmux send-keys -t 1 "colcon build --symlink-install" C-m
+tmux send-keys -t 1 "source install/setup.bash" C-m
+tmux send-keys -t 1 "ros2 launch isaac_ros_visual_slam isaac_ros_visual_slam_realsense.launch.py"
 
 
 tmux select-window -t main
 #----------------------------------------------------------------------------
-tmux attach -t nef_issac_vio # needed to run
+tmux attach -t agipix_test # needed to run
